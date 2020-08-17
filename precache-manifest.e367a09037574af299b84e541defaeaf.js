7659727601630e0bef47b9d88d0e48cca7f42854
Bernhard Grünewaldt <bernhard.gruenewaldt@comsysto.com>
2020-08-17 16:42:22 +0200

    Updates

---
+++ precache-manifest.e367a09037574af299b84e541defaeaf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e63a8df47d503c36ab5e3a92dd94fa1",
+    "url": "/Display-Override-PropertyList-File-Parser-and-Generator-with-HiDPI-Support-For-Scaled-Resolutions/index.html"
+  },
+  {
+    "revision": "4d49efefe9211a25070d",
+    "url": "/Display-Override-PropertyList-File-Parser-and-Generator-with-HiDPI-Support-For-Scaled-Resolutions/static/css/2.fcdf98cf.chunk.css"
+  },
+  {
+    "revision": "f906774aaaa4b28a3c00",
+    "url": "/Display-Override-PropertyList-File-Parser-and-Generator-with-HiDPI-Support-For-Scaled-Resolutions/static/css/main.7fd33d29.chunk.css"
+  },
+  {
+    "revision": "4d49efefe9211a25070d",
+    "url": "/Display-Override-PropertyList-File-Parser-and-Generator-with-HiDPI-Support-For-Scaled-Resolutions/static/js/2.958d81eb.chunk.js"
+  },
+  {
+    "revision": "209edf4585d9f58f9ad6afde7880261f",
+    "url": "/Display-Override-PropertyList-File-Parser-and-Generator-with-HiDPI-Support-For-Scaled-Resolutions/static/js/2.958d81eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f906774aaaa4b28a3c00",
+    "url": "/Display-Override-PropertyList-File-Parser-and-Generator-with-HiDPI-Support-For-Scaled-Resolutions/static/js/main.e8740030.chunk.js"
+  },
+  {
+    "revision": "dc8ef5d634d5dab1ec25",
+    "url": "/Display-Override-PropertyList-File-Parser-and-Generator-with-HiDPI-Support-For-Scaled-Resolutions/static/js/runtime-main.d556b9dd.js"
+  },
+  {
+    "revision": "d23430321de2f2ffc7044cbae14e7505",
+    "url": "/Display-Override-PropertyList-File-Parser-and-Generator-with-HiDPI-Support-For-Scaled-Resolutions/static/media/comsysto-logo.d2343032.png"
+  },
+  {
+    "revision": "64f60ba8cad2eff649359a98a6bea452",
+    "url": "/Display-Override-PropertyList-File-Parser-and-Generator-with-HiDPI-Support-For-Scaled-Resolutions/static/media/logo-scaled-resolutions.64f60ba8.svg"
+  }
+]);
--- src/common/components/XmlParserError.js
+++
@@ -1,8 +0,0 @@
-export default class XmlParserError extends Error {
-  constructor(message) {
-    super();
-    this.message = message;
-    this.stack = (new Error()).stack;
-    this.name = this.constructor.name;
-  }
-}
